1092b5505f926d60321fc0dbc13a8374a6306fba
Cristian Rojas <crarojasca@unal.edu.co>
2023-04-26 13:48:34 +1000

    cards_augmented

--- binary_classification/inference.py
+++ binary_classification/inference.py
@@ -1,4 +1,5 @@
 import os
+import pickle
 import pandas as pd
 
 
@@ -19,13 +20,11 @@ parser = HfArgumentParser((ModelArguments, DataTrainingArguments, EvalArguments,
 model_args, data_args, eval_args, training_args = parser.parse_json_file(json_file="train.json")
 model_dir = Path(training_args.output_dir).joinpath(model_args.save_name)
 
-
 MAX_LEN = 256
 BATCH_SIZE = 4
 MODEL_NAME = os.path.basename(training_args.output_dir)
 
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
-
 
 print("Loading model: {}".format(model_dir))
 tokenizer = AutoTokenizer.from_pretrained(model_dir)
@@ -45,13 +44,16 @@ dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=False)
 #     model, dataloader
 # )
 
+with open('../cards/models/label_encoder.pkl', 'rb') as f:
+    le = pickle.load(f)
+
 predictions = []
 scores = []
 for batch in tqdm(dataloader):
     outputs = model(**batch)
     score = outputs.logits.softmax(dim = 1)
     prediction = torch.argmax(outputs.logits, axis=1)
-    predictions += prediction.tolist()
+    predictions += le.inverse_transform(prediction.to('cpu') + 1).tolist()
     scores += score.tolist()
 
 data[f"{MODEL_NAME}_pred"] = predictions
